62b5624d7a84f4c7f26b49da1066ee1eca9f9ac0
Vinícius de Andrade Barros <viniciusdeandrade96@gmail.com>
2023-01-29 13:52:36 -0300

    Creation of OrderLocation model and the interface OrderRepository

---
+++ src/main/java/br/com/fiap/abctechapi/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package br.com.fiap.abctechapi.repository;
+
+import br.com.fiap.abctechapi.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
